0bfd9365a130b5a38202160d4dcd3d725e307a67
David M. Rivero G <davidmriverog@gmail.com>
2016-03-30 22:48:59 -0430

    compilamos less a css con grunt

--- eje-less-grunt-1/Gruntfile.js
+++ eje-less-grunt-1/Gruntfile.js
@@ -7,11 +7,23 @@ module.exports = function(grunt) {
         src: 'src/js/*.js',
         dest: 'dist/js/operations.min.js'
       }
+    },
+    less:{
+      development:{
+        options:{
+          paths:['src/less/']
+        },
+        files:{
+          'dist/css/styles.min.css':'src/less/styles.less'
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
